420a95a397780ed2bb52e3a378b3adbf8654f099
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-04 16:20:49 +0530

    Assignment23

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace ConAppAssignment23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Login Page");
+                LoginOperation.login();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!! " + ex.Message);
+            }
+            finally { Console.ReadKey(); }
+
+        }
+    }
+}
